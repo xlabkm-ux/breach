65e053095cf943879889135d8f7b2dbd02ed15e2
LabKM <xlabkm@gmail.com>
2026-04-06 07:56:08 +0300

    feat(mission): add ObjectiveService objective flags

---
+++ Assets/Scripts/Mission/ObjectiveService.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+namespace Breach.Mission
+{
+    public sealed class ObjectiveService : MonoBehaviour
+    {
+        [Header("Primary Objective Flags")]
+        [SerializeField] private bool infiltrationComplete;
+        [SerializeField] private bool hostageFreed;
+        [SerializeField] private bool hostageExtracted;
+
+        [Header("Fail Conditions")]
+        [SerializeField] private bool squadAlive = true;
+        [SerializeField] private bool hostageAlive = true;
+
+        public bool InfiltrationComplete => infiltrationComplete;
+        public bool HostageFreed => hostageFreed;
+        public bool HostageExtracted => hostageExtracted;
+        public bool SquadAlive => squadAlive;
+        public bool HostageAlive => hostageAlive;
+
+        public bool IsMissionFailed => !squadAlive || !hostageAlive;
+        public bool IsMissionSuccessCandidate => hostageExtracted && hostageAlive && squadAlive;
+
+        public void MarkInfiltrationComplete()
+        {
+            infiltrationComplete = true;
+        }
+
+        public void MarkHostageFreed()
+        {
+            hostageFreed = true;
+        }
+
+        public void MarkHostageExtracted()
+        {
+            hostageExtracted = true;
+        }
+
+        public void MarkSquadWiped()
+        {
+            squadAlive = false;
+        }
+
+        public void MarkHostageKilled()
+        {
+            hostageAlive = false;
+        }
+    }
+}
